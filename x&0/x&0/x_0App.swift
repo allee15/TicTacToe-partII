35bd480a0d958682fdf7802f54bf8d45099eef3c
Alexia Aldea <alexia.aldea@mready.net>
2024-04-22 14:14:37 +0300

    first commit

---
+++ x&0/x&0/x_0App.swift
@@ -0,0 +1,17 @@
+//
+//  x_0App.swift
+//  x&0
+//
+//  Created by Alexia Aldea on 22.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct x_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
